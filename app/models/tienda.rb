ca70814850e0461f4cab9711eea489d5cc2e446d
ivanacostarubio <ivan@bakedweb.net>
2009-12-15 15:45:36 -0500

    format_address

--- app/models/tienda.rb
+++ app/models/tienda.rb
@@ -1,5 +1,10 @@
 class Tienda < ActiveRecord::Base
+  before_save :format_address
   acts_as_mappable :auto_geocode => {:field => :direccion }
   
  #validates_presence_of :nombre, :direccion, :ciudad, :estado, :codigo_postal, :pais
+ 
+ def format_address
+  self.direccion = self.direccion + " " + self.ciudad + " " + self.estado + " " + self.pais
+ end
 end
